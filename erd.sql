4cbf64e4ddc4265b4e197f4957bc13c52f8c9eee
FernRuelas0000 <fruelas2000@gmail.com>
2026-04-14 20:14:14 -0700

    Enforce NOT NULL and CHECK constraints in tables
    Added NOT NULL constraints to email, password_hash, role_id, check_in_date, and check_out_date fields. Added CHECK constraint to ensure check_out_date is after check_in_date.

--- erd.sql
+++ erd.sql
@@ -6,21 +6,22 @@ CREATE TABLE user_roles (
 
 CREATE TABLE users ( 
   user_id SERIAL PRIMARY KEY,
-  email TEXT UNIQUE,
-  password_hash TEXT,
+  email TEXT UNIQUE NOT NULL,
+  password_hash TEXT NOT NULL,
   first_name TEXT,
   last_name TEXT,
   phone TEXT,
-  role_id INT,
+  role_id INT NOT NULL,
   FOREIGN KEY (role_id) REFERENCES user_roles(role_id)
 );
 
 CREATE TABLE reservations (
   reservation_id SERIAL PRIMARY KEY,
-  user_id INT,
-  check_in_date DATE,
-  check_out_date DATE,
+  user_id INT NOT NULL,
+  check_in_date DATE NOT NULL,
+  check_out_date DATE NOT NULL,
   event_type TEXT,
   description TEXT,
-  FOREIGN KEY (user_id) REFERENCES users(user_id)
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  CHECK(check_out_date > check_in_date)
 );
