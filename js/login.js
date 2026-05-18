d79d607e96c1a694296343dfcb482e5257d6f075
ltn015 <nguyenttleo@gmail.com>
2026-05-17 20:14:35 -0700

    Added "Enter" login functionality as well as minor admin page stylization
    Added event listener to email and password fields of login.html to allow form submission using the "Enter" key for both fields. Added elementary divs for layout of the Admin Dashboard as well as navbar and button styles to match index.html.

--- js/login.js
+++ js/login.js
@@ -3,6 +3,8 @@ const loginBtn = document.getElementById("loginBtn");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 
+const loginFields = document.querySelectorAll('.login-field');
+
 const message = document.getElementById("message");
 
 loginBtn.addEventListener("click", function () {
@@ -55,4 +57,61 @@ loginBtn.addEventListener("click", function () {
     window.location.href =
       "index.html";
   }, 500);
+});
+
+loginFields.forEach(field => {
+  field.addEventListener("keydown", function () {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      const username = email.value.trim();
+      const userPassword = password.value.trim();
+
+      if (username === "" || userPassword === "") {
+        message.textContent =
+          "Please fill in both fields.";
+
+        return;
+      }
+
+      // Admin login redirects to admin page
+      if (
+        username === "admin" &&
+        userPassword === "admin"
+      ) {
+        sessionStorage.setItem(
+          "adminLoggedIn",
+          "true"
+        );
+
+        sessionStorage.setItem(
+          "facultyLoggedIn",
+          "true"
+        );
+
+        message.textContent =
+          "Admin login successful!";
+
+        setTimeout(() => {
+          window.location.href =
+            "admin.html";
+        }, 500);
+
+        return;
+      }
+
+      // Standard login redirects to index and enables create reservation button
+      sessionStorage.setItem(
+        "facultyLoggedIn",
+        "true"
+      );
+
+      message.textContent =
+        "Login successful!";
+
+      setTimeout(() => {
+        window.location.href =
+          "index.html";
+      }, 500);
+    }
+  });
 });
